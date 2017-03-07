fc97b8993c2ab976f7bfcd2bdea410535c9f366c
Holodeck <ljgdavie@dundee.ac.uk>
2017-03-07 17:02:58 +0000

    Starting scene - cube changes y position relative to user's head.

---
+++ Assets/Scripts/InitialCubeYPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialCubeYPosition : MonoBehaviour {
+
+    //To get position of head
+    public GameObject eyeCamera;
+    Vector3 eyeCameraTransPosition;
+
+    // Use this for initialization
+    void Start () {
+ 
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Get current position of user's head on the y axis
+        eyeCameraTransPosition = eyeCamera.transform.position;
+        float eyeCameraYPosition = eyeCameraTransPosition.y;
+
+        //Vary start cube's y position relative to the user's head
+        Vector3 cubePosition = new Vector3(1.005f, eyeCameraYPosition-0.3f, -0.099f);
+        this.gameObject.transform.position = cubePosition;
+
+    }
+}
