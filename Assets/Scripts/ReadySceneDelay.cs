f71eaa796ebb02addc07231d28b4d543c922e0f0
Holodeck <ljgdavie@dundee.ac.uk>
2017-03-21 18:25:06 +0000

    Created a 'you are ready' scene which might be cool. Not in the build yet, however.

---
+++ Assets/Scripts/ReadySceneDelay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadySceneDelay : MonoBehaviour
+{
+
+    GameObject manager;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        manager = GameObject.Find("GameManager");
+        StartCoroutine(beginTransition());
+    }
+
+
+    IEnumerator beginTransition()
+    {
+        //Wait a few seconds
+        //Bit longer than initial teleport because we wanna listen to the loop for a bit
+        yield return new WaitForSeconds(4.0f);
+
+        //Tell GameManager to do scene change
+        manager.GetComponent<GameManager>().nextScene();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
